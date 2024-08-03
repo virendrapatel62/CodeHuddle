f667c90e5c7a1c6e3b6413a25627a7bdee5d3b80
Virendra Patel <virendrapatel@koloapp.in>
2024-08-03 15:44:06 +0530

    Added project setup

---
+++ src/env.ts
@@ -0,0 +1,7 @@
+const env = {
+  appWrite: {
+    endPoint: String(process.env.NEXT_PUBLIC_APPWRITE_HOST_URL),
+    projectId: String(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID),
+    apiKey: String(process.env.APPWRITE_API_KEY),
+  },
+};
