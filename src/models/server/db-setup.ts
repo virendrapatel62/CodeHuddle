8bb20fe3b27a6663c612712db3d9383bf92cf0c6
Virendra Patel <virendrapatel@koloapp.in>
2024-08-03 17:35:08 +0530

    Added db setup

---
+++ src/models/server/db-setup.ts
@@ -0,0 +1,33 @@
+import { db } from "../name";
+import createAnswerCollection from "./answer.collection";
+import createCommentCollection from "./comment.collection";
+import { databases } from "./config";
+import createQuestionCollection from "./question.collection";
+import createVoteCollection from "./vote.collection";
+
+export default async function getOrCreateDB() {
+  try {
+    await databases.get(db);
+    console.log("Database connection");
+  } catch (error) {
+    try {
+      await databases.create(db, db);
+      console.log("Database created");
+
+      //creating Collections
+      await Promise.all([
+        createQuestionCollection(),
+        createAnswerCollection(),
+        createCommentCollection(),
+        createVoteCollection(),
+      ]);
+
+      console.log("Collections created");
+      console.log("Database connected");
+    } catch (error) {
+      console.log("Error creating databases or collections");
+    }
+
+    return databases;
+  }
+}
